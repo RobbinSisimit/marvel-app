9f8f92ba373c762890e279e6ad7740d852adc02a
RobbinSisimit <rsisimit-2023485@kinal.edu.gt>
2024-09-05 22:08:36 -0600

    se agrego cuadro de texto y boton

--- src/components/MarvelApp.jsx
+++ src/components/MarvelApp.jsx
@@ -5,12 +5,18 @@ import { useState } from 'react';
 
 export const MarvelApp = () => {
   const [page, setPage] = useState()
+  const [name,setnam] = useState()
+
+ 
   
+  console.log(name)
   //console.log(characters)
   const {characters} = useCharacters(page)
   return (
     <>
         <div>
+        <input className="form-control me-2" type="text" onChange={(e/*evento*/) => {setnam(e.target.value)}}/>
+        <input className="btn btn-outline-success" type ="submit" value="Buscar character"/>
           <GridCharacters characters={characters}/>
           <Pagination count={10} variant="outlined" color="primary" onChange={(e, value) => setPage(value)}/>
         </div>
